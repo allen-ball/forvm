899dd66c4599cfaa0f6dd8a5e02bc779048bd476
Allen D. Ball <ball@hcf.dev>
2018-08-24 20:13:30 +0000

    Interim check-in: Use Flyway to manage database schema.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/forvm/trunk@3250 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/resources/db/migration/V1_0__Schema.sql
@@ -0,0 +1,93 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for osx10.13 (x86_64)
+--
+-- Host: localhost    Database: forvm
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `articles` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `slug` varchar(255) NOT NULL,
+  `markdown` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_sn7al9fwhgtf98rvn8nxhjt4f` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `authors`
+--
+
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `authors` (
+  `email` varchar(64) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `title` varchar(12) DEFAULT NULL,
+  `first` varchar(32) DEFAULT NULL,
+  `middle` varchar(32) DEFAULT NULL,
+  `last` varchar(32) DEFAULT NULL,
+  `suffix` varchar(12) DEFAULT NULL,
+  `alias` longtext,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `UK_50058x3s3i9lcreapeth7bbsw` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tags` (
+  `name` varchar(32) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `UK_sn0d91hxu700qcw0n4pebp5vc` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+--
+-- Table structure for table `credentials`
+--
+
+DROP TABLE IF EXISTS `credentials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `credentials` (
+  `email` varchar(64) NOT NULL,
+  `password` longtext NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-08-24 13:04:09
