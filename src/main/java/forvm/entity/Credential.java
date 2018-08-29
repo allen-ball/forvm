eafe74022c89578abed61f3c8e4ee7a9e41fcb96
Allen D. Ball <ball@hcf.dev>
2018-08-29 02:46:37 +0000

    Added javax.validation.constraints annotations.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/forvm/trunk@3274 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/forvm/entity/Credential.java
+++ src/main/java/forvm/entity/Credential.java
@@ -11,6 +11,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,9 +33,11 @@ public class Credential extends JSONBean {
 
     @Getter @Setter
     @Id @Column(length = 64, nullable = false, unique = true)
+    @NotBlank @Email
     private String email = null;
 
     @Getter @Setter
     @Lob @Column(nullable = false)
+    @NotBlank
     private String password = null;
 }
