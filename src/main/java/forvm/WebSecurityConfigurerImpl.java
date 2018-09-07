36f978eed3174a117e9ec1a0271eae0ebfadc2d9
Allen D. Ball <ball@hcf.dev>
2018-09-07 18:03:25 +0000

    Require HTTP Basic Authentication for /api/**.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/forvm/trunk@3302 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/forvm/WebSecurityConfigurerImpl.java
+++ src/main/java/forvm/WebSecurityConfigurerImpl.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,7 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * {@link org.springframework.security.config.annotation.web.WebSecurityConfigurer}
- * implementation
+ * abstract base class
  *
  * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
  * @version $Revision$
@@ -28,34 +29,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfigurerImpl extends WebSecurityConfigurerAdapter {
+public abstract class WebSecurityConfigurerImpl
+                      extends WebSecurityConfigurerAdapter {
     private static final Logger LOGGER = LogManager.getLogger();
 
     @Autowired private UserDetailsService userDetailsService;
     @Autowired private PasswordEncoder passwordEncoder;
 
-    /**
-     * Sole constructor.
-     */
-    public WebSecurityConfigurerImpl() { super(); }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-            .antMatchers("/css/**", "/js/**", "/images/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .anyRequest()
-            .permitAll();
-        http.formLogin()
-            .loginPage("/login")
-            .permitAll();
-        http.logout()
-            .permitAll();
-    }
+    private WebSecurityConfigurerImpl() { super(); }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -65,4 +46,54 @@ public class WebSecurityConfigurerImpl extends WebSecurityConfigurerAdapter {
 
     @Override
     public String toString() { return super.toString(); }
+
+    /**
+     * {@link org.springframework.security.config.annotation.web.WebSecurityConfigurer}
+     * implementation for the API
+     */
+    @Configuration
+    @Order(1)
+    public static class API extends WebSecurityConfigurerImpl {
+
+        /**
+         * Sole constructor.
+         */
+        public API() { super(); }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/api/**")
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and().httpBasic();
+        }
+    }
+
+    /**
+     * {@link org.springframework.security.config.annotation.web.WebSecurityConfigurer}
+     * implementation for the UI
+     */
+    @Configuration
+    @Order(2)
+    public static class UI extends WebSecurityConfigurerImpl {
+
+        /**
+         * Sole constructor.
+         */
+        public UI() { super(); }
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring()
+                .antMatchers("/css/**", "/js/**", "/images/**");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .authorizeRequests().anyRequest().permitAll()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
+        }
+    }
 }
