1454921eb7e94f8ef37610797ab6c864f2cb4725
Allen D. Ball <ball@hcf.dev>
2018-10-22 15:15:42 +0000

    Initial wriitng.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/forvm/trunk@3361 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ src/main/java/forvm/ChangePasswordForm.java
@@ -0,0 +1,35 @@
+/*
+ * $Id$
+ *
+ * Copyright 2018 Allen D. Ball.  All rights reserved.
+ */
+package forvm;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Backing bean for "change password" form
+ *
+ * {@bean.info}
+ *
+ * @author {@link.uri mailto:ball@iprotium.com Allen D. Ball}
+ * @version $Revision$
+ */
+@NoArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class ChangePasswordForm {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private String username;
+    private String password;
+    private String newPassword;
+    private String repeatPassword;
+}
