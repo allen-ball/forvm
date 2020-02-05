40a28f3d9079a2c59c9802257198fa375920ffbc
Allen D. Ball <ball@hcf.dev>
2020-02-05 04:23:21 +0000

    Generated license-maven-plugin:update-file-header headers.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/forvm/trunk@5285 b3e4a033-f9ea-474d-ac63-5dcbede9114a

--- src/main/java/forvm/ant/taskdefs/MarkdownTask.java
+++ src/main/java/forvm/ant/taskdefs/MarkdownTask.java
@@ -1,10 +1,25 @@
-/*
- * $Id$
- *
- * Copyright 2018, 2019 Allen D. Ball.  All rights reserved.
- */
 package forvm.ant.taskdefs;
-
+/*-
+ * ##########################################################################
+ * forvm Blog Publishing Platform
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2018 - 2020 Allen D. Ball
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ##########################################################################
+ */
 import ball.swing.table.MapTableModel;
 import ball.util.ant.taskdefs.AnnotatedAntTask;
 import ball.util.ant.taskdefs.AntTask;
